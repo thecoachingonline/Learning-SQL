6363f2efda7182b1702afd8147c7667e6a59574d
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-24 01:44:31 +0000

    Tables

---
+++ Creating-Tables.sql
@@ -0,0 +1,6 @@
+create table DEPARTMENTS (  
+  deptno        number,  
+  name          varchar2(50) not null,  
+  location      varchar2(50),  
+  constraint pk_departments primary key (deptno)  
+);
